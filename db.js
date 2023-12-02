b235ccfd91dc7985cbfed34d3c88dbf9079d46dd
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-12-02 20:57:28 +0530

    deploy library

---
+++ db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+require('dotenv').config()
+const connection = mongoose.connect(process.env.URL)
+
+
+const bookSchema = mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    genre: { type: String, required: true },
+}, {
+    versionKey: false
+})
+
+const BookModel = mongoose.model("book", bookSchema)
+
+module.exports = {
+    connection,
+    BookModel
+}
